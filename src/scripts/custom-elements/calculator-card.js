97441049ea9ef834395a58463c8b31347244aeda
Moritz Kröger <write@morkro.de>
2015-07-13 23:41:06 +0200

    📐 added CalculatorCard math logic

--- src/scripts/custom-elements/calculator-card.js
+++ src/scripts/custom-elements/calculator-card.js
@@ -7,6 +7,9 @@
 	var CalculatorCard	= Object.create(HTMLDivElement.prototype);
 
 	CalculatorCard.config = { module: true, type: 'calculator', content: {} };
+	CalculatorCard.decimalAdded = false;
+	CalculatorCard.timeOut = 2000;
+	CalculatorCard.hasNoDigits = /[^\d.\-\+()/*÷×\s]/g;
 
 	/**
 	 * @description	Initialises data.
@@ -44,6 +47,98 @@
 	CalculatorCard.createdCallback = function () {
 		this.root = this.createShadowRoot();
 		this.root.appendChild( document.importNode(template.content, true) );
+
+		// input view
+		this.mathResult = this.root.querySelector('#calc-input');
+		this.mathResult.addEventListener('keydown', this.evalCalcSubmit.bind(this));
+		// button
+		this.options = this.root.querySelector('.calc-options');
+		this.options.addEventListener('click', this.evalCalcOptions.bind(this));
+	};
+
+	CalculatorCard.getButton = function (elem) {
+		if (elem && elem.nodeName === 'BUTTON') {
+			return elem;
+		} else if (elem && elem.nodeName === 'SVG-ICON') {
+			return elem.parentNode;
+		} else {
+			return false;
+		}
+	};
+
+	CalculatorCard.hasClass = function (elem, name) {
+		return elem && elem.classList.contains('calc-' + name);
+	};
+
+	CalculatorCard.evalCalcSubmit = function (event) {
+		const ENTER = 13;
+		if (event.keyCode === ENTER) {
+			event.preventDefault();
+			this.calculateMathResult();
+		}
+	};
+
+	CalculatorCard.evalCalcOptions = function (event) {
+		event.preventDefault();
+		var elem = event.target;
+		var btn = this.getButton(elem);
+		var btnType = btn.getAttribute('data-calculator');
+
+		// If backspace is pressed
+		if (btnType === 'backspace') {
+			this.backspaceMath(event);
+			this.decimalAdded = false;
+		}
+		// If clear is pressed
+		else if (btnType === 'clear') {
+			this.clearMath(event);
+			this.decimalAdded = false;
+		}
+		// If decimal is added
+		else if (btnType === 'decimal') {
+			if (!this.decimalAdded) {
+				this.displayCalculatorValue('.');
+				this.decimalAdded = true;
+			}
+		}
+		// Result
+		else if (btnType === 'equal') {
+			this.calculateMathResult();
+		}
+		else {
+			this.displayCalculatorValue(btnType);
+		}
+	};
+
+	CalculatorCard.calculateMathResult = function () {
+		var result = this.mathResult.value;
+		var cleanResult = result.replace(/[^0-9.\-\+()×÷/*]/g, '').replace(/×/g, '*').replace(/÷/g, '/');
+
+		if (this.hasNoDigits.test(result)) {
+			this.mathResult.value = `I only calculate digits!`;
+			setTimeout(this.clearMath.bind(this), this.timeOut);
+		}
+		else {
+			// This might be a valid use case for 'eval()'.
+			// I know it's considered evil, but I don't want to include
+			// a bloated library nor writing my own parser for that.
+			// I'm not sending data anywhere anyway. If you still
+			// think I'm doing something really bad, please drop me a line
+			// and I rethink my decision!
+			this.mathResult.value = eval(cleanResult);
+		}
+	};
+
+	CalculatorCard.clearMath = function () {
+		this.mathResult.value = null;
+	};
+
+	CalculatorCard.backspaceMath = function () {
+		this.mathResult.value = this.mathResult.value.slice(0, -1);
+	};
+
+	CalculatorCard.displayCalculatorValue = function (value) {
+		this.mathResult.value += value;
 	};
 
 	/* Register element in document */
